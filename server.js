4ef80ceba66333ff0674c126974ac053f77acaef
tahaboufeldja499-cell <tahaboufeldja499@gmail.com>
2026-09-02 21:39:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 
 const path = require('path');
+const fs = require('fs');
 const crypto = require('crypto');
 const express = require('express');
 const cookieParser = require('cookie-parser');
@@ -25,7 +26,12 @@ if (ADMIN_CODE === 'CHANGE_THIS_ADMIN_CODE' || SESSION_SECRET === 'CHANGE_THIS_S
 // ---------------------------------------------------------------------------
 // Database setup
 // ---------------------------------------------------------------------------
-const DB_PATH = path.join(__dirname, 'data', 'restaurant.db');
+const DATA_DIR = path.join(__dirname, 'data');
+if (!fs.existsSync(DATA_DIR)) {
+  fs.mkdirSync(DATA_DIR, { recursive: true });
+}
+
+const DB_PATH = path.join(DATA_DIR, 'restaurant.db');
 const db = new Database(DB_PATH);
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
